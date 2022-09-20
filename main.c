c5528ab7954599e237d28054c6c0b33dd87f86f4
pswan0103 <22zch@naver.com>
2022-09-20 17:23:12 +0900

    3

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+
 int main(void)
 {
 	char text;
@@ -10,6 +11,5 @@ int main(void)
 	printf("input a character:");
 	scanf("%c", &text);
 	
-	printf("The next character of %c (%d) is %c (%d)\n", text, text, text+1);	
-	return 0;
+	printf("The next character of %c (%d) is %c (%d)\n", text, text, text+1, text+1);	
 }
